cf1fa08daec59ea4c6d5ced6e7e7255bce5d7660
suabicii <michal.slabik3@wp.pl>
2021-05-27 17:10:55 +0200

    Added menu bar

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,9 +1,14 @@
 package org.example.controller;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ResourceBundle;
@@ -32,5 +37,32 @@ public class MainController {
             e.printStackTrace();
         }
         borderPane.setCenter(parent);
+    }
+
+    @FXML
+    public void closeApplication() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    @FXML
+    public void setCaspian() {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_CASPIAN);
+    }
+
+    @FXML
+    public void setModena() {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
+    }
+
+    @FXML
+    public void setAlwaysOnTop(ActionEvent actionEvent) {
+        Stage stage = (Stage) borderPane.getScene().getWindow();
+        boolean value = ((CheckMenuItem) actionEvent.getSource()).selectedProperty().get();
+        stage.setAlwaysOnTop(value);
+    }
+
+    @FXML
+    public void about() {
     }
 }
