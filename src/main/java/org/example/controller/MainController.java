469dbeb29d90b075109db72a12685502754aedd6
suabicii <michal.slabik3@wp.pl>
2021-05-27 16:26:30 +0200

    Added button for adding books

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -1,7 +1,12 @@
 package org.example.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class MainController {
 
@@ -14,5 +19,18 @@ public class MainController {
     @FXML
     private void initialize() {
         topMenuButtonsController.setMainController(this);
+    }
+
+    public void setCenter(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+        ResourceBundle bundle = ResourceBundle.getBundle("org.example.bundles.messages");
+        loader.setResources(bundle);
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(parent);
     }
 }
