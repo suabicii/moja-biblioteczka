33e588152dd998268c68733787eb296ba0b87813
suabicii <michal.slabik3@wp.pl>
2021-05-27 14:22:17 +0200

    Added resource bundles and internationalization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,26 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+//        Locale.setDefault(new Locale("en"));
         FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/BorderPaneMain.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("org.example.bundles.messages");
+        loader.setResources(bundle);
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Moja biblioteczka");
+        primaryStage.setTitle(bundle.getString("title.application"));
         primaryStage.show();
     }
 
